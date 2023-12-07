b7e829cc946e902dd603e49dd12ae434f445c328
Kayetic <bartek.bak@protonmail.com>
2023-12-07 13:19:57 +0000

    file saving on background thread

--- Plain Text Editor/AppDelegate.swift
+++ Plain Text Editor/AppDelegate.swift
@@ -14,15 +14,24 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate, Observable
 
     func saveDocument() {
         if let url = currentFileURL {
-            // Save directly if there is an existing file URL
-            do {
-                try text.write(to: url, atomically: true, encoding: .utf8)
-                hasUnsavedChanges = false
-                updateWindowTitleClosure?()
-            } catch {
-                print("Failed to save document to existing file: \(error.localizedDescription)")
-            }
-        } else {
+                // Perform file writing in the background
+                DispatchQueue.global(qos: .userInitiated).async {
+                    do {
+                        try self.text.write(to: url, atomically: true, encoding: .utf8)
+                        
+                        // Switch back to the main thread to update the UI
+                        DispatchQueue.main.async {
+                            self.hasUnsavedChanges = false
+                            self.updateWindowTitleClosure?()
+                        }
+                    } catch {
+                        DispatchQueue.main.async {
+                            print("Failed to save document to existing file: \(error.localizedDescription)")
+                        }
+                    }
+                }
+            } else {
+
             // If no file URL, show save panel to choose location
             let panel = NSSavePanel()
             panel.allowedContentTypes = [UTType.plainText]
