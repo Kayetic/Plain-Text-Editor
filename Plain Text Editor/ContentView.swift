1b5658f2b2596bdccbf65dc2522354e8cd7988d2
Kayetic <bartek.bak@protonmail.com>
2023-12-07 10:24:42 +0000

    Initial Commit

---
+++ Plain Text Editor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Plain Text Editor
+//
+//  Created by Bartek Bak on 07/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Plain Text Editor/Plain_Text_EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Plain_Text_EditorApp.swift
+//  Plain Text Editor
+//
+//  Created by Bartek Bak on 07/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Plain_Text_EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
